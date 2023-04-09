6e2f4db3007490fb087c9bd7086e47640a7b523b
ducksaucewithmsg <104735875+ducksaucewithmsg@users.noreply.github.com>
2023-04-09 10:40:55 -0500

    Add files via upload

---
+++ rules.mk
@@ -0,0 +1,39 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+ENCODER_ENABLE = yes
+CONSOLE_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+VIA_ENABLE = yes
+LTO_ENABLE = yes
+RGBLIGHT_ENABLE = yes
+
+OCEAN_DREAM_ENABLE = yes
+LUNA_ENABLE = yes
+
+WPM_ENABLE = yes
+
+ifeq ($(strip $(OLED_DRIVER_ENABLE)), yes)
+    SRC += oled_setup.c
+
+	ifdef OCEAN_DREAM_ENABLE
+		ifeq ($(strip $(OCEAN_DREAM_ENABLE)), yes)
+			SRC += ocean_dream.c
+			OPT_DEFS += -DOCEAN_DREAM_ENABLE
+    	endif
+	endif
+	ifndef OCEAN_DREAM_ENABLE
+		SRC += ocean_dream.c
+		OPT_DEFS += -DOCEAN_DREAM_ENABLE
+	endif
+
+	ifdef LUNA_ENABLE
+		ifeq ($(strip $(LUNA_ENABLE)), yes)
+			SRC += luna.c
+			OPT_DEFS += -DLUNA_ENABLE
+		endif
+	endif
+	ifndef LUNA_ENABLE
+		SRC += luna.c
+		OPT_DEFS += -DLUNA_ENABLE
+	endif
+endif
